68e4cd8803ff0275a24a2fbed41f1ccf320e61f9
Amit Budhodkar <116152541+amit1725@users.noreply.github.com>
2024-03-14 19:13:39 +0530

    Add files via upload

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import Spinner from './Spinner'
+
+const Blogs = () => {
+  
+  const {posts,loading}=useContext(AppContext)
+
+  return (
+    <div className='w-11/12 max-w-[670px] py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px]'>
+      {
+        loading?(<Spinner/>):(
+            posts.length===0?
+            (
+              <div>
+                <p>No Post Found</p>
+              </div>
+            )
+            :
+            (
+              posts.map((post)=>(
+                <div key={post.id}>
+                   <p className='font-bold text-lg'>{post.title}</p>
+                   <p className='text-sm'>
+                    By <span className='italic'>{post.author}</span> on <span className='underline'>{post.category}</span>
+                   </p>
+                   <p className='text-sm mt-[4px]'>
+                    Posted on <span>{post.date}</span>
+                   </p>
+                   <p className='text-md mt-[14px]'>
+                    {post.content}
+                   </p>
+                   <div className='flex gap-x-3'>
+                    {post.tags.map((tag,index)=>{
+                        return <span key={index} 
+                        className='text-blue-500 underline font-bold text-xs mt-[5px]'>{` #${tag}`}</span>
+                    })}
+                   </div>
+                </div>
+              ))
+            )
+        )
+      }
+    </div>
+  )
+}
+
+export default Blogs
